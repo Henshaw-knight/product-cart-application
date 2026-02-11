7a7c39e26156a6fdc999b7d349affc7b04073cad
Henshaw J. Essien <josephhensh2000@gmail.com>
2026-02-11 15:08:42 +0100

    Routes setup

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
+import { ProductList } from '../components/product-list/product-list';
+import { ProductDetail } from '../components/product-detail/product-detail';
+import { Cart } from '../components/cart/cart';
+import { NotFound } from '../components/not-found/not-found';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', component: ProductList },
+    { path: 'products', component: ProductList },
+    { path: 'products/:id', component: ProductDetail },
+    { path: 'cart', component: Cart },
+    { path: '404', component: NotFound },
+    { path: '**', redirectTo: '404' }
+];
