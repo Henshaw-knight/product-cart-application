b34bce4483e24a5ff731575ff21a896a473f391a
Henshaw J. Essien <josephhensh2000@gmail.com>
2026-03-10 14:27:02 +0100

    Update routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,13 +4,21 @@ import { ProductDetail } from '../components/product-detail/product-detail';
 import { Cart } from '../components/cart/cart';
 import { NotFound } from '../components/not-found/not-found';
 import { ProductForm } from '../components/product-form/product-form';
+import { Login } from '../components/login/login';
+import { authGuard } from '../guards/auth-guard';
 
 export const routes: Routes = [
-    { path: '', component: ProductList },
-    { path: 'products', component: ProductList },
-    { path: 'products/new', component: ProductForm },
-    { path: 'products/:id', component: ProductDetail },
-    { path: 'cart', component: Cart },
+    // Public route - no guard
+    { path: 'login', component: Login},
+
+    // Protected routes - require authentication
+    { path: '', component: ProductList, canActivate: [authGuard] },
+    { path: 'products', component: ProductList, canActivate: [authGuard] },
+    { path: 'products/new', component: ProductForm, canActivate: [authGuard] },
+    { path: 'products/:id', component: ProductDetail, canActivate: [authGuard] },
+    { path: 'cart', component: Cart, canActivate: [authGuard] },
+
+    // Public routes - no guard
     { path: '404', component: NotFound },
     { path: '**', redirectTo: '404' }
 ];
