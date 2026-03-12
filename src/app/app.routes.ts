b744d96f43d14da3a12997528ba8d1d4beb7cfff
Henshaw J. Essien <josephhensh2000@gmail.com>
2026-03-12 09:40:17 +0100

    perf: implement lazy loading for all routes with preloading strategy

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -8,17 +8,43 @@ import { Login } from '../components/login/login';
 import { authGuard } from '../guards/auth-guard';
 
 export const routes: Routes = [
-    // Public route - no guard
-    { path: 'login', component: Login},
+    // Public route - no guard (Lazy loaded)
+    { path: 'login',
+      loadComponent: () => import('../components/login/login')
+        .then(m => m.Login)
+    },
 
-    // Protected routes - require authentication
-    { path: '', component: ProductList, canActivate: [authGuard] },
-    { path: 'products', component: ProductList, canActivate: [authGuard] },
-    { path: 'products/new', component: ProductForm, canActivate: [authGuard] },
-    { path: 'products/:id', component: ProductDetail, canActivate: [authGuard] },
-    { path: 'cart', component: Cart, canActivate: [authGuard] },
+    // Protected routes - require authentication (Lazy loaded)
+    { path: '',
+      loadComponent: () => import('../components/product-list/product-list')
+        .then(m => m.ProductList), canActivate: [authGuard]
+    },
+
+    { path: 'products',
+      loadComponent: () => import('../components/product-list/product-list')
+        .then(m => m.ProductList), canActivate: [authGuard]
+    },
+
+    { path: 'products/new',
+      loadComponent: () => import('../components/product-form/product-form')
+        .then(m => m.ProductForm), canActivate: [authGuard]
+    },
+
+    { path: 'products/:id',
+      loadComponent: () => import('../components/product-detail/product-detail')
+        .then(m => m.ProductDetail), canActivate: [authGuard]
+    },
+
+    { path: 'cart',
+      loadComponent: () => import('../components/cart/cart')
+        .then(m => m.Cart), canActivate: [authGuard]
+    },
 
     // Public routes - no guard
-    { path: '404', component: NotFound },
+    { path: '404',
+      loadComponent: () => import('../components/not-found/not-found')
+        .then(m => m.NotFound)
+    },
+
     { path: '**', redirectTo: '404' }
 ];
