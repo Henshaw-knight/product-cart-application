ee8458442c7ed2880e3c681230854181e5be9de0
Henshaw J. Essien <josephhensh2000@gmail.com>
2026-07-18 00:34:31 +0100

    fix: set client render mode for dynamic routes and update angular runtime

--- src/app/app.routes.server.ts
+++ src/app/app.routes.server.ts
@@ -2,6 +2,10 @@ import { RenderMode, ServerRoute } from '@angular/ssr';
 
 export const serverRoutes: ServerRoute[] = [
   {
+    path: 'products/:id',
+    renderMode: RenderMode.Client, // render on client, not prerendered
+  },
+  {
     path: '**',
     renderMode: RenderMode.Prerender
   }
